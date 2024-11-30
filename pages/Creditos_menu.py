34574cb8c77675f01cc9c78478c9680b55ac9ec3
AlToor <torresruizalan3@gmail.com>
2024-11-30 15:33:26 -0600

    Update Creditos_menu.py

--- pages/Creditos_menu.py
+++ pages/Creditos_menu.py
@@ -17,10 +17,12 @@ import time  # time.sleep(1)
 import streamlit as st
 import numpy as np
 import pandas as pd
+import pathlib
 
 from main_konigsberg import menu
 # ------------------------- Variables -------------------------
 # Time
+
 # ---------------- Configuracion de la pagina -----------------
 
 st.set_page_config(
@@ -28,18 +30,54 @@ st.set_page_config(
     page_icon="🗒️",
     layout="wide", 
     initial_sidebar_state="auto"
-
 )
 
 menu()
 
+# ---------------- Configuracion CSS --------------------------
+
+def load_css(file_path):
+    with open(file_path) as f:
+        st.html(f"<style>{f.read()}</style>")
+
+css_path = pathlib.Path("assets/style.css")
+load_css(css_path)
+
 # --------------------------- Code ---------------------------
 
-st.title("Creditos")
+st.markdown("<div class='titulo'><h1 class='creditos'>Creditos.</h1></div>", unsafe_allow_html=True)
+
+st.divider()
 
 st.markdown(
     """
-    ## Tareas realizadas por cada integrante del equipo
-    "Nombre" fue responsable del area de "area", realizando la tarea de "tarea"
+    ### :green[:material/grid_view:] Diseño de UI
+    :green[**Página principal**]  
+    * Alan Torres Ruiz  
+    * Paola Montserrat Osorio García  
+
+    :green[**Interacción con el grafo**] 
+    * Vanessa Reteguín  
+    * Martín Isaí Núñez Villeda  
+    ### :blue[:material/account_tree:] Construcción de grafos
+    :blue[**Implementación de nodos**]
+    * Alexis Alberto Zúñiga Alonso  
+    * Pablo David Pérez López  
+
+    :blue[**Implementación de arcos**]  
+    * Abel López Rosales  
+    * Jesús Yocsan Luévano Flores
+    ### :violet[:material/browse_activity:] Funcionamiento de menús
+    :violet[**Backend**]
+    * Eduardo Isaí Lopez García  
+    * Alan Fernando Martinez Moreno  
+
+    :violet[**Administración de archivos**] 
+    * Zahid Ríos Durón  
+    * Jesús Emmanuel Saucedo Solís
     """
 )
+
+st.divider()
+
+st.caption("Derechos reservados de uso para el Equipo 1")
